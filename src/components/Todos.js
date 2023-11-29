15ae866dcc5efbac93c59a179f6f265009ca1205
Marco <cwhong8087@gmail.com>
2023-11-29 14:13:44 +0900

    “updated”

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -25,6 +25,15 @@ const Todos = ({
         <input />
         <button type="submit">등록</button>
       </form>
+      <div>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
   );
 };
+
+export default Todos;
